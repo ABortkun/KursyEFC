cf14000cfb55253ec15cc8ea717b3c7b47556742
ABort1 <a.bortkun@jmblab.com>
2021-11-17 14:21:18 +0100

    Added logger to log exceptions and return current status code

--- Building your first api with ASP.NET Core/CityInfo.API/Controllers/PointsOfInterestController.cs
+++ Building your first api with ASP.NET Core/CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using CityInfo.API.Models;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace CityInfo.API.Controllers
 {
@@ -12,6 +13,26 @@ namespace CityInfo.API.Controllers
     [Route("api/cities/{cityId}/pointsofinterest")]
     public class PointsOfInterestController : ControllerBase
     {
+        private readonly ILogger<PointsOfInterestController> _logger;
+
+        public PointsOfInterestController(ILogger<PointsOfInterestController> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet]
+        public IActionResult GetPointsOfInterest(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+            {
+                _logger.LogInformation($"City with id {cityId} wasn't found when accessing points of interest.");
+                return NotFound();
+            }
+
+            return Ok(city.PointsOfInterest);
+        }
         [HttpGet("{id}", Name="GetPointOfInterest")]
         public IActionResult GetPointsOfInterest(int cityId, int id)
         {
