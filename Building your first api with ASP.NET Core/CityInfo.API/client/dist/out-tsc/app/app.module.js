b9c6fcde2f3c647e8b58d1fc41170ae63aba43e5
ABort1 <a.bortkun@jmblab.com>
2021-11-25 12:10:47 +0100

    Added welcome component to display welcome page(doesn't work)

--- Building your first api with ASP.NET Core/CityInfo.API/client/dist/out-tsc/app/app.module.js
+++ Building your first api with ASP.NET Core/CityInfo.API/client/dist/out-tsc/app/app.module.js
@@ -2,12 +2,14 @@ import { __decorate } from "tslib";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 let AppModule = class AppModule {
 };
 AppModule = __decorate([
     NgModule({
         declarations: [
-            AppComponent
+            AppComponent,
+            WelcomeComponent
         ],
         imports: [
             BrowserModule
