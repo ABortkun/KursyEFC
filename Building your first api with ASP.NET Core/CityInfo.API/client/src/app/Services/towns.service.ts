d12c9bb9fcffa6d2e52ea8937d5b6536c3f8b11b
ABort1 <a.bortkun@jmblab.com>
2021-11-29 12:55:47 +0100

    Added Type safety

--- Building your first api with ASP.NET Core/CityInfo.API/client/src/app/Services/towns.service.ts
+++ Building your first api with ASP.NET Core/CityInfo.API/client/src/app/Services/towns.service.ts
@@ -1,20 +1,22 @@
 ﻿import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
+import { City } from "../shared/City";
 
 @Injectable()
 export class Towns {
 
-    public cities: any[] = [];
+    public cities: City[] = [];
 
     constructor(private http: HttpClient) {
 
     }
 
-    loadCities() {
+    loadCities(): Observable<void> {
        return this.http.get<any[]>("/api/cities/citiesOnly")
        .pipe(map(data => {
-           this.cities = <any[]>(data);
+           this.cities = data;
            return;
        }))
     }
---
+++ Building your first api with ASP.NET Core/CityInfo.API/client/src/app/shared/City.ts
@@ -0,0 +1,5 @@
+﻿export class City {
+    id!: number;
+    name!: string;
+    description!: string;
+}
