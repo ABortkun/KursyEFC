eeaaa9e6ac5e1617bc1db4ef2ded7eb42ae59c7f
ABort1 <a.bortkun@jmblab.com>
2021-12-29 11:54:16 +0100

    adjust highlighting to selected option from the list

--- Building your first api with ASP.NET Core/CityInfo.API/client/src/app/Components/sidenav/sidenav.component.ts
+++ Building your first api with ASP.NET Core/CityInfo.API/client/src/app/Components/sidenav/sidenav.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from "../../Services/DataService";
 import { Service } from "../../Services/service";
+import { ICity }  from "../../shared/City";
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: 'sidenav.component.html',
-  styles: [
-  ]
+  styleUrls: [ 'sidenav.component.css']
 })
 export class SidenavComponent implements OnInit {
+    selectedIndex: number | null;
 
-  constructor(public dataservice: DataService, public service: Service) { }
+    constructor(public dataservice: DataService, public service: Service) { }
 
   ngOnInit(): void {
               this.service.apiCitiesCitiesOnlyGet()
@@ -18,4 +19,12 @@ export class SidenavComponent implements OnInit {
                 this.dataservice.cities = res;
             });
     }
+
+    onSelected(_index: number): void {
+        this.selectedIndex = _index;
+    }
+    MainTableSelected(): void {
+        this.selectedIndex = null;
+    }
+
  }
--- Building your first api with ASP.NET Core/CityInfo.API/client/src/app/Services/DataService.ts
+++ Building your first api with ASP.NET Core/CityInfo.API/client/src/app/Services/DataService.ts
@@ -1,5 +1,14 @@
 ﻿import { ICity }  from "../shared/City";
+import { Subject } from 'rxjs/Subject';
 
 export class DataService {
     cities: ICity[] = [];
+
+    private selectedCitySource = new Subject();
+    selectedProductChanges$ = this.selectedCitySource.asObservable();
+
+
+    changeSelectedCity(selectedCity: ICity | null): void {
+        this.selectedCitySource.next(selectedCity);
+    }
 }
